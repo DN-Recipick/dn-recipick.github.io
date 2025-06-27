8760c14f620f796c6d493c2b69fa3aa98416a32a
SanghyeonLee <hyeon012366@gmail.com>
2025-06-28 00:32:41 +0900

    fix: base URL을 GitHub 링크에서 상대 경로로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/DN-Recipick/recipick-frontend',
+  base: '/recipick-frontend/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
