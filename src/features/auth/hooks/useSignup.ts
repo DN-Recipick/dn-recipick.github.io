ce9af0e9287a229e4be069f905a401eebdad11e8
SanghyeonLee <hyeon012366@gmail.com>
2025-06-29 04:28:10 +0900

    feat: useSignup 훅에서 SignupPayload 타입 사용 및 성공 시 처리 로직 추가

--- src/features/auth/hooks/useSignup.ts
+++ src/features/auth/hooks/useSignup.ts
@@ -1,24 +1,41 @@
+import { SUCCESS_MESSAGES } from '@/constants/messages';
+import { ROUTES } from '@/constants/routes';
 import { signup } from '@/features/auth/apis';
+import type { SignupPayload } from '@/types/api';
+import { showToast } from '@/utils/toast';
 import { signupSchema, type SignupForm } from '@/validation/auth.schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 export const useSignup = () => {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<SignupForm>({
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordCheck: '',
+    },
     resolver: zodResolver(signupSchema),
     mode: 'onBlur',
   });
 
   const { isPending, mutate } = useMutation({
-    mutationFn: (formData: SignupForm) => signup(formData),
+    mutationFn: (formData: SignupPayload) => signup(formData),
+    onSuccess: () => {
+      showToast.success(SUCCESS_MESSAGES.auth.login);
+      navigate(ROUTES.SIGNIN);
+    },
   });
 
-  const onSubmit = handleSubmit((formData) => mutate(formData));
+  const onSubmit = handleSubmit(({ email, password }) => {
+    mutate({ email, password });
+  });
 
   return { register, isPending, onSubmit, errors };
 };
