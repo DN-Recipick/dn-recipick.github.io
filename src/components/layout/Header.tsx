2d5d369a980e546d9e274ede9aa01cc81ff57d1b
SanghyeonLee <hyeon012366@gmail.com>
2025-06-29 04:26:44 +0900

    feat: 헤더 컴포넌트 리팩토링 및 드롭다운 메뉴 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,36 +1,65 @@
-import Button from '@/components/shared/Button';
-import ThemeDropdown from '@/components/layout/ThemeDropdown';
 import { ROUTES } from '@/constants/routes';
-import { useSignout } from '@/features/auth/hooks/useSignout';
 import { useNavigate } from 'react-router-dom';
-import { GoSignOut, GoSignIn, GoPersonAdd } from 'react-icons/go';
 import Logo from '@/assets/logo.svg';
 import Image from '@/components/shared/Image';
+import { useAuthStore } from '@/store/useAuthStore';
+import Button from '@/components/shared/Button';
+import Dropdown from '@/components/layout/Dropdown/Dropdown';
+import { THEME_OPTIONS, USER_OPTIONS } from '@/constants/dropdownOptions';
+import { useThemeStore } from '@/store/useThemeStore';
+import { FaUserAlt } from 'react-icons/fa';
+import DropdownList from '@/components/layout/Dropdown/DropdownList';
+import { useSignout } from '@/features/auth/hooks/useSignout';
+import DropdownItems from '@/components/layout/Dropdown/RenderDropdownItems';
 
 const Header = () => {
+  const { isSignin } = useAuthStore();
   const navigate = useNavigate();
-  const signout = useSignout();
+  const { theme, setTheme } = useThemeStore();
+  const handleSignout = useSignout();
+
   return (
     <header className="w-full p-content flex-center-between z-30">
-      <Image src={Logo} onClick={() => navigate(ROUTES.HOME)} wrapClassName="w-12 cursor-pointer" />
       <div className="flex-center gap-2">
-        <ThemeDropdown />
-        <Button onClick={signout} className="hover:bg-[var(--color-bg-hover)] rounded p-1.5">
-          <GoSignOut className="mini-icon-size" />
-        </Button>
-        <Button
-          onClick={() => navigate(ROUTES.SIGNIN)}
-          className="hover:bg-[var(--color-bg-hover)] rounded p-1.5"
-        >
-          <GoSignIn className="mini-icon-size" />
-        </Button>
-        <Button
-          onClick={() => navigate(ROUTES.SIGNUP)}
-          className="hover:bg-[var(--color-bg-hover)] rounded p-1.5"
-        >
-          <GoPersonAdd className="mini-icon-size" />
-        </Button>
+        <Image
+          src={Logo}
+          onClick={() => navigate(ROUTES.HOME)}
+          wrapClassName="w-12 cursor-pointer"
+        />
+        <Dropdown
+          buttonInner={THEME_OPTIONS.find((o) => o.text === theme)?.icon}
+          dropdownList={({ setOpen }) => (
+            <DropdownList listPosition="right">
+              <DropdownItems
+                options={THEME_OPTIONS}
+                setOpen={setOpen}
+                onSelect={(option) => setTheme(option.text)}
+              />
+            </DropdownList>
+          )}
+        />
       </div>
+      {isSignin ? (
+        <Dropdown
+          buttonInner={<FaUserAlt />}
+          dropdownList={({ setOpen }) => (
+            <DropdownList listPosition="left">
+              <DropdownItems
+                options={USER_OPTIONS}
+                setOpen={setOpen}
+                onSelect={(option) => {
+                  if (option.text === '로그아웃') handleSignout();
+                }}
+              />
+            </DropdownList>
+          )}
+        />
+      ) : (
+        <div className="flex gap-2">
+          <Button onClick={() => navigate(ROUTES.SIGNIN)} className="btn-primary" text="로그인" />
+          <Button onClick={() => navigate(ROUTES.SIGNUP)} className="btn-primary" text="회원가입" />
+        </div>
+      )}
     </header>
   );
 };
