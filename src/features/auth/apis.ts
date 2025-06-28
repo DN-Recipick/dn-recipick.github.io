6f53fcf24e552a1f20fbe35bc9e611667278a225
SanghyeonLee <hyeon012366@gmail.com>
2025-06-29 04:27:58 +0900

    fix: AUTH SIGNIN 및 SIGNUP 함수에서 불필요한 코드 제거

--- src/features/auth/apis.ts
+++ src/features/auth/apis.ts
@@ -1,15 +1,9 @@
 import { ENDPOINTS } from '@/constants/endPoints';
 import httpClient from '@/lib/httpClient';
-import { type SignupForm, type SigninForm } from '@/validation/auth.schema';
+import type { SigninResponse, SignupPayload } from '@/types/api';
+import type { SigninForm } from '@/validation/auth.schema';
 
-const SUPABASE_API = import.meta.env.VITE_API_SUPABASE;
+export const signin = (payload: SigninForm): Promise<SigninResponse> =>
+  httpClient.post(ENDPOINTS.AUTH.SIGNIN, payload);
 
-export const signin = (payload: SigninForm) =>
-  httpClient.post(ENDPOINTS.AUTH.SIGNIN, payload, {
-    baseURL: SUPABASE_API,
-  });
-
-export const signup = (payload: SignupForm) =>
-  httpClient.post(ENDPOINTS.AUTH.SIGNUP, payload, {
-    baseURL: SUPABASE_API,
-  });
+export const signup = (payload: SignupPayload) => httpClient.post(ENDPOINTS.AUTH.SIGNUP, payload);
