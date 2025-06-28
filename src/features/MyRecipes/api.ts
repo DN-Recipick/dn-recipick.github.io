1558b26cdb6b0955f41c1fc342630b89809c7097
SanghyeonLee <hyeon012366@gmail.com>
2025-06-29 04:28:14 +0900

    feat: MyRecipes API에 getRecipes 함수 추가

---
+++ src/features/MyRecipes/api.ts
@@ -0,0 +1,7 @@
+import { ENDPOINTS } from '@/constants/endPoints';
+import httpClient from '@/lib/httpClient';
+import type { RecipeListResponse } from '@/types/recipe';
+import { getAuthHeaders } from '@/utils/auth';
+
+export const getRecipes = (): Promise<RecipeListResponse> =>
+  httpClient.get(ENDPOINTS.RECIPE.ADD, { headers: getAuthHeaders(), mode: 'cors' });
