eaf11f0a1b017385f6f269315321a3f35c626d2d
SanghyeonLee <hyeon012366@gmail.com>
2025-06-29 04:28:06 +0900

    feat: useSignout 훅에 queryClient.clear() 호출 추가

--- src/features/auth/hooks/useSignout.ts
+++ src/features/auth/hooks/useSignout.ts
@@ -2,16 +2,19 @@ import { useCallback } from 'react';
 import { useAuthStore } from '@/store/useAuthStore';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '@/constants/routes';
+import { useQueryClient } from '@tanstack/react-query';
 
 export const useSignout = () => {
+  const queryClient = useQueryClient();
   const clearUser = useAuthStore((s) => s.clearUser);
   const navigate = useNavigate();
 
   const signout = useCallback(() => {
     localStorage.removeItem('token');
     clearUser();
+    queryClient.clear();
     navigate(ROUTES.HOME);
-  }, [clearUser, navigate]);
+  }, [clearUser, navigate, queryClient]);
 
   return signout;
 };
