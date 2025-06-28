4236f2f0897e61133b816f0eb94e5557576e1389
SanghyeonLee <hyeon012366@gmail.com>
2025-06-29 04:28:02 +0900

    feat: useSignin 훅에 성공 시 처리 로직 추가 및 기본값 설정

--- src/features/auth/hooks/useSignIn.ts
+++ src/features/auth/hooks/useSignIn.ts
@@ -1,21 +1,41 @@
+import { SUCCESS_MESSAGES } from '@/constants/messages';
+import { ROUTES } from '@/constants/routes';
 import { signin } from '@/features/auth/apis';
+import { useAuthStore } from '@/store/useAuthStore';
+import type { SigninResponse } from '@/types/api';
+import { showToast } from '@/utils/toast';
 import { signinSchema, type SigninForm } from '@/validation/auth.schema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 export const useSignin = () => {
+  const queryClient = useQueryClient();
+  const { setUser } = useAuthStore();
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<SigninForm>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
     resolver: zodResolver(signinSchema),
     mode: 'onBlur',
   });
 
   const { isPending, mutate } = useMutation({
     mutationFn: (formData: SigninForm) => signin(formData),
+    onSuccess: (res: SigninResponse) => {
+      localStorage.setItem('token', res?.access_token);
+      setUser(res.user);
+      queryClient.clear();
+      showToast.success(SUCCESS_MESSAGES.auth.login);
+      navigate(ROUTES.HOME);
+    },
   });
 
   const onSubmit = handleSubmit((formData) => mutate(formData));
