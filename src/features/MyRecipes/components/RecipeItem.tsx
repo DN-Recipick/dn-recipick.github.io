6a0aeff93bb94d42f0612be30c9a6a0cf36013e1
SanghyeonLee <hyeon012366@gmail.com>
2025-06-29 04:28:29 +0900

    feat: RecipeItem 컴포넌트 추가

---
+++ src/features/MyRecipes/components/RecipeItem.tsx
@@ -0,0 +1,31 @@
+import { ROUTES } from '@/constants/routes';
+import type { Recipe } from '@/types/recipe';
+import { formatDateTime } from '@/utils/format';
+import { useNavigate } from 'react-router-dom';
+
+const RecipeItem = ({ recipe }: { recipe: Recipe }) => {
+  const navigate = useNavigate();
+  return (
+    <article
+      className="h-full cursor-pointer bg-white dark:bg-gray-800 rounded-lg overflow-hidden shadow-[var(--shadow)] flex-column
+            transition-transform duration-200 hover:scale-[1.02] hover:shadow-lg"
+      onClick={() =>
+        navigate(ROUTES.RECIPE(recipe.id), {
+          state: { recipe },
+        })
+      }
+    >
+      <div className="bg-amber-900 h-35" />
+      <div className="p-3 flex-1 flex-column justify-between">
+        <h3 className="text-lg font-extrabold line-clamp-2">{recipe.name}</h3>
+        <div className="flex-column gap-2">
+          <p className="text font-bold line-clamp-2">{recipe.title}</p>
+          <p className="text-sm font-bold text-gray-400">{recipe.channel}</p>
+          <p className="text-sm text-gray-400">{formatDateTime(recipe.saved_at)}</p>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default RecipeItem;
