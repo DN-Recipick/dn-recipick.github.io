83246a87f491f2c6e505531566bce4fb9e07dc33
SanghyeonLee <hyeon012366@gmail.com>
2025-06-29 04:27:53 +0900

    feat: ROUTES에 RECIPES, RECIPE, RECIPE_PATH 추가

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,5 +2,8 @@ export const ROUTES = {
   HOME: '/',
   SIGNIN: '/login',
   SIGNUP: '/signup',
+  RECIPES: '/recipes',
+  RECIPE: (id: string) => `/recipes/:${id}}`,
+  RECIPE_PATH: '/recipes/:id',
   ALL: '*',
 };
