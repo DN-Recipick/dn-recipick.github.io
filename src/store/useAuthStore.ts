4d9c43578fdfce875d3462b3c993e98a324c545e
SanghyeonLee <hyeon012366@gmail.com>
2025-06-29 04:28:57 +0900

    fix: isSignin 상태 관리 추가 및 setUser, clearUser 로직 수정

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -4,6 +4,7 @@ import type { User } from '@/types/user';
 
 interface AuthState {
   user: User | null;
+  isSignin: boolean;
   setUser: (user: User) => void;
   clearUser: () => void;
 }
@@ -12,8 +13,9 @@ export const useAuthStore = create<AuthState>()(
   persist(
     (set) => ({
       user: null,
-      setUser: (user) => set({ user }),
-      clearUser: () => set({ user: null }),
+      isSignin: false,
+      setUser: (user) => set({ user, isSignin: true }),
+      clearUser: () => set({ user: null, isSignin: false }),
     }),
     {
       name: 'auth-user', // localStorage에 저장될 key 이름
