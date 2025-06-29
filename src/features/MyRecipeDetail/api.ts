e5b1490d711be3fcd1b10558819dd7d51a7435ff
SanghyeonLee <hyeon012366@gmail.com>
2025-06-30 04:21:22 +0900

    feat: 레시피 아이템 조회 기능 추가

--- src/features/MyRecipeDetail/api.ts
+++ src/features/MyRecipeDetail/api.ts
@@ -1,10 +1,16 @@
 import { ENDPOINTS } from '@/constants/endPoints';
 import httpClient from '@/lib/httpClient';
 import type { KurlyItemResponse } from '@/types/kurly';
+import type { RecipeItemResponse } from '@/types/recipe';
 import { getAuthHeaders } from '@/utils/auth';
 
 export const getKurlyItems = (keyword: string): Promise<KurlyItemResponse> =>
   httpClient.get(ENDPOINTS.INGREDIENT, {
     queryParams: { keyword },
+    headers: getAuthHeaders(),
+  });
+
+export const getRecipeItem = (id: string): Promise<RecipeItemResponse> =>
+  httpClient.get(ENDPOINTS.RECIPE.ITEM(id), {
     headers: getAuthHeaders(),
   });
