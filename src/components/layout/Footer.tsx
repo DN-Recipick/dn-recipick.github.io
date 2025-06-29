4b02d3f37511dc049d67c65f92e69ccd6ec8de7f
SanghyeonLee <hyeon012366@gmail.com>
2025-06-30 00:30:01 +0900

    feat: Footer 컴포넌트에 테마 선택 드롭다운 추가

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,28 @@
+import Dropdown from '@/components/layout/Dropdown/Dropdown';
+import DropdownList from '@/components/layout/Dropdown/DropdownList';
+import DropdownItems from '@/components/layout/Dropdown/RenderDropdownItems';
+import { THEME_OPTIONS } from '@/constants/dropdownOptions';
+import { useThemeStore } from '@/store/useThemeStore';
+
 const Footer = () => {
+  const { theme, setTheme } = useThemeStore();
   return (
-    <footer className="flex-center w-full p-content">
+    <footer className="flex-center-between gap-2 w-full p-content">
       <small className="sm:text-[0.7rem] text-[0.6rem]">
         © 2025 Sanghyeon Lee. All rights reserved.
       </small>
+      <Dropdown
+        buttonInner={THEME_OPTIONS.find((o) => o.text === theme)?.icon}
+        dropdownList={({ setOpen }) => (
+          <DropdownList className="bottom-8 right-0">
+            <DropdownItems
+              options={THEME_OPTIONS}
+              setOpen={setOpen}
+              onSelect={(option) => setTheme(option.text)}
+            />
+          </DropdownList>
+        )}
+      />
     </footer>
   );
 };
