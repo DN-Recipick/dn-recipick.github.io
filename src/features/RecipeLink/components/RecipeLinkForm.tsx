1506ec1de4de2ae58092862a492c03883d604692
SanghyeonLee <hyeon012366@gmail.com>
2025-06-30 04:21:54 +0900

    feat: RecipeLinkForm에서 에러 메시지 표시 로직 제거 및 코드 정리

--- src/features/RecipeLink/components/RecipeLinkForm.tsx
+++ src/features/RecipeLink/components/RecipeLinkForm.tsx
@@ -1,23 +1,21 @@
 import Button from '@/components/shared/Button';
 import Input from '@/components/shared/Input';
 import { useLinkRecipe } from '@/features/RecipeLink/hooks/useLinkRecipe';
-import { showToast } from '@/utils/toast';
-import { useEffect } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 
 const RecipeLinkForm = () => {
   const { register, isPending, onSubmit, errors } = useLinkRecipe();
-  useEffect(() => {
-    if (errors.url?.message) {
-      showToast.error(errors.url.message);
-    }
-  }, [errors.url?.message]);
   return (
     <form
       onSubmit={onSubmit}
-      className="p-2 flex-column justify-between border-[0.1rem] border-[var(--color-primary)] shadow-[var(--shadow)] min-h-25 rounded-md"
+      className="p-2 my-5 flex-column justify-between border-[0.1rem] border-[var(--color-primary)] shadow-[var(--shadow)] min-h-25 rounded-md"
     >
-      <Input {...register('url')} className="focus:outline-0" placeholder="유튜브 영상 링크 입력" />
+      <Input
+        {...register('url')}
+        error={errors.url?.message}
+        className="focus:outline-0"
+        placeholder="유튜브 영상 링크 입력"
+      />
       <div className="flex flex-row-reverse">
         <Button type="submit" isPending={isPending} className="btn-primary rounded-md">
           <FaArrowRight className="text-[0.9rem]" />
