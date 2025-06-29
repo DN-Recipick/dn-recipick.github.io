f4d515d9a89337110d738c612de29178cb2e42f6
SanghyeonLee <hyeon012366@gmail.com>
2025-06-30 04:43:31 +0900

    feat: Vite 설정에서 기본 경로를 '/dn-recipick/'로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/dn-recipick/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
