2fa5d6c5ac67a6a8cb037e6f71ce1bf950676005
SanghyeonLee <hyeon012366@gmail.com>
2025-06-30 00:31:44 +0900

    feat: useGetKurlyItem 훅 추가하여 재료 조회 기능 구현

---
+++ src/features/MyRecipeDetail/hooks/useGetKurlyItem.ts
@@ -0,0 +1,12 @@
+import { queryKeys } from '@/constants/queryKeys';
+import { getKurlyItems } from '@/features/MyRecipeDetail/api';
+import { useQuery } from '@tanstack/react-query';
+
+export const useGetKurlyItem = (ingredientKeyword: string) => {
+  const { data, isPending } = useQuery({
+    queryKey: queryKeys.KURLY_ITEM(ingredientKeyword),
+    queryFn: () => getKurlyItems(ingredientKeyword),
+  });
+
+  return { kurlyItems: data?.result, isPending };
+};
