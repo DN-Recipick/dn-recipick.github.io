9338b15862750a259c33ad09c603a1e638235dab
SanghyeonLee <hyeon012366@gmail.com>
2025-06-30 00:30:20 +0900

    refactor: DropdownListProps에서 listPosition 제거 및 className으로 대체

--- src/components/layout/Dropdown/DropdownList.tsx
+++ src/components/layout/Dropdown/DropdownList.tsx
@@ -2,15 +2,15 @@ import clsx from 'clsx';
 import { type ReactNode } from 'react';
 
 export interface DropdownListProps {
-  listPosition: 'right' | 'left';
+  className: string;
   children: ReactNode;
 }
 
-const DropdownList = ({ listPosition, children }: DropdownListProps) => {
+const DropdownList = ({ className, children }: DropdownListProps) => {
   return (
     <ul
       className={clsx(
-        listPosition === 'right' ? 'left-0' : 'right-0',
+        className,
         'absolute overflow-hidden bg-white dark:bg-gray-800 mt-2 border border-gray-600 shadow-[var(--shadow)] rounded z-10',
       )}
     >
