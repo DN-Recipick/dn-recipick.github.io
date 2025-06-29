9dfa51a584b176d6501a9b563f4acdac334a6c62
SanghyeonLee <hyeon012366@gmail.com>
2025-06-30 00:31:22 +0900

    fix: RECIPE 경로의 템플릿 문자열 수정

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,7 +3,7 @@ export const ROUTES = {
   SIGNIN: '/login',
   SIGNUP: '/signup',
   RECIPES: '/recipes',
-  RECIPE: (id: string) => `/recipes/:${id}}`,
+  RECIPE: (id: string) => `/recipes/${id}`,
   RECIPE_PATH: '/recipes/:id',
   ALL: '*',
 };
