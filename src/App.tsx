0e84346bbf8540f13109b3a9f5971304a00fdf16
SanghyeonLee <hyeon012366@gmail.com>
2025-06-30 00:29:52 +0900

    feat: Modal 컴포넌트를 추가하고 상태에 따라 표시

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { useThemeStore } from '@/store/useThemeStore';
 import { Outlet } from 'react-router-dom';
 import { useEffect } from 'react';
 import Footer from '@/components/layout/Footer';
+import Modal from '@/components/shared/Modal';
+import { useModalStore } from '@/store/useModalStore';
 
 function App() {
   const initTheme = useThemeStore((state) => state.initTheme);
-
+  const { isOpen } = useModalStore();
   useEffect(() => {
     initTheme();
   }, [initTheme]);
@@ -17,6 +19,7 @@ function App() {
       <main className="flex-1 w-full px-content py-10">
         <Outlet />
       </main>
+      {isOpen && <Modal />}
       <Footer />
     </>
   );
