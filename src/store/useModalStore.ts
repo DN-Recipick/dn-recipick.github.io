eae9932aeed1e13bc937e78ac66eea2314781bf9
SanghyeonLee <hyeon012366@gmail.com>
2025-06-30 00:32:19 +0900

    feat: useModalStore 훅 추가하여 모달 상태 관리 기능 구현

---
+++ src/store/useModalStore.ts
@@ -0,0 +1,24 @@
+import { create } from 'zustand';
+
+export type ModalType = 'ingredient' | null;
+
+interface ModalData {
+  title: string;
+  [key: string]: unknown;
+}
+
+interface ModalState {
+  isOpen: boolean;
+  type: ModalType;
+  data: ModalData | null;
+  open: (type: ModalType, data?: ModalData | null) => void;
+  close: () => void;
+}
+
+export const useModalStore = create<ModalState>((set) => ({
+  isOpen: false,
+  type: null,
+  data: null,
+  open: (type, data = null) => set({ isOpen: true, type, data }),
+  close: () => set({ isOpen: false, type: null, data: null }),
+}));
