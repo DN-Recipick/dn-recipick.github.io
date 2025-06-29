cfd01b9ea30b69e4f395309763a9d91e4edb6a59
SanghyeonLee <hyeon012366@gmail.com>
2025-06-30 04:22:03 +0900

    feat: RecipeLinkHeading 컴포넌트 제거 및 홈 페이지 텍스트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,17 @@
+import Button from '@/components/shared/Button';
+import { ROUTES } from '@/constants/routes';
 import RecipeLinkForm from '@/features/RecipeLink/components/RecipeLinkForm';
-import RecipeLinkHeading from '@/features/RecipeLink/components/RecipeLinkHeading';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className="full-page-center">
       <div className="max-w-xl w-full">
-        <RecipeLinkHeading />
+        <h2>영상으로 쉽게 레시피를 알아보세요</h2>
         <RecipeLinkForm />
+        <Link to={ROUTES.RECIPES}>
+          <Button className="btn-primary w-full" text="내 레시피" />
+        </Link>
       </div>
     </div>
   );
