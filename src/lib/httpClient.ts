dc1ed4f9f2680e47a93d42dd50bb16257c5f4617
SanghyeonLee <hyeon012366@gmail.com>
2025-06-30 22:50:26 +0900

    fix: 기본값을 제거하여 HTTP 메서드의 데이터 인자를 명확히 함

--- src/lib/httpClient.ts
+++ src/lib/httpClient.ts
@@ -3,9 +3,9 @@ import type { HttpClient } from '@/types/api';
 
 const httpClient: HttpClient = {
   get: (endpoint, options) => request('GET', endpoint, undefined, options),
-  post: (endpoint, data = undefined, options) => request('POST', endpoint, data, options),
-  put: (endpoint, data = undefined, options) => request('PUT', endpoint, data, options),
-  patch: (endpoint, data = undefined, options) => request('PATCH', endpoint, data, options),
-  delete: (endpoint, data = undefined, options) => request('DELETE', endpoint, data, options),
+  post: (endpoint, data, options) => request('POST', endpoint, data, options),
+  put: (endpoint, data, options) => request('PUT', endpoint, data, options),
+  patch: (endpoint, data, options) => request('PATCH', endpoint, data, options),
+  delete: (endpoint, data, options) => request('DELETE', endpoint, data, options),
 };
 export default httpClient;
