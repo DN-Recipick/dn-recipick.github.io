a17cee906696dca365f7a34c98bfd97130b8aff2
SanghyeonLee <hyeon012366@gmail.com>
2025-06-30 22:50:55 +0900

    fix: Supabase API 키를 가져오는 함수 추가

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,3 +1,9 @@
+const API_SUPABASE_KEY = import.meta.env.VITE_API_SUPABASE_KEY;
+
 export const getAuthHeaders = () => ({
   Authorization: `Bearer ${localStorage.getItem('token') ?? ''}`,
+});
+
+export const getSupabaseApiKey = () => ({
+  apikey: API_SUPABASE_KEY,
 });
