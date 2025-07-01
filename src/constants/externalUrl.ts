daf206c66816eb4caec254147e2490c9f9a89259
SanghyeonLee <hyeon012366@gmail.com>
2025-07-01 23:35:17 +0900

    feat: KURLY_ITEM_URL 및 YOUTUBE_THUMBNAIL 상수 추가

---
+++ src/constants/externalUrl.ts
@@ -0,0 +1,3 @@
+export const KURLY_ITEM_URL = (no: string) => `https://www.kurly.com/goods/${no}`;
+export const YOUTUBE_THUMBNAIL = (id: string) =>
+  `https://img.youtube.com/vi/${id}/maxresdefault.jpg`;
