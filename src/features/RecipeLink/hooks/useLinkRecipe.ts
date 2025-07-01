0845d0b5f700b64c0615d3c741ae05140b8071e1
SanghyeonLee <hyeon012366@gmail.com>
2025-07-01 23:35:56 +0900

    feat: useLinkRecipe 훅에서 navigate 추가 및 console.log 제거

--- src/features/RecipeLink/hooks/useLinkRecipe.ts
+++ src/features/RecipeLink/hooks/useLinkRecipe.ts
@@ -1,10 +1,13 @@
+import { ROUTES } from '@/constants/routes';
 import { addRecipe } from '@/features/RecipeLink/api';
 import { recipeLinkScheme, type RecipeLink } from '@/validation/recipeLink.schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 export const useLinkRecipe = () => {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -18,8 +21,8 @@ export const useLinkRecipe = () => {
   });
 
   const onSubmit = handleSubmit((formData) => {
-    console.log(formData);
     mutate(formData);
+    navigate(ROUTES.RECIPES);
   });
 
   return { register, isPending, onSubmit, errors };
