c00057d3ccec4527aa1faf81fc49b2ef6cfbea73
SanghyeonLee <hyeon012366@gmail.com>
2025-07-04 01:47:41 +0900

    feat: Logo 컴포넌트 추가 및 기본 스타일 설정

---
+++ src/components/shared/Logo.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import { ROUTES } from '@/constants/routes';
+import Image from '@/components/shared/Image';
+import LogoImg from '@/assets/logo.svg';
+
+const Logo = () => {
+  return (
+    <Link to={ROUTES.HOME} className="flex-center gap-2 rounded-xl hover-opacity">
+      <Image src={LogoImg} alt={LogoImg} wrapClassName="w-12 cursor-pointer" />
+      <p
+        className="
+    text-xl font-extrabold text-transparent bg-clip-text 
+    bg-gradient-to-r 
+    from-[#18644f] via-[#25947a] to-[#36b49a]
+    dark:from-[#268f72] dark:via-[#43b99d] dark:to-[#a7f3d0]
+  "
+      >
+        RECIPICK
+      </p>
+    </Link>
+  );
+};
+
+export default Logo;
