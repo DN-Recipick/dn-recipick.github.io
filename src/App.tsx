277dc2d091d0a086abf162100748662eb4eb9ec2
SanghyeonLee <hyeon012366@gmail.com>
2025-07-03 22:27:01 +0900

    feat: useFallbackStore 추가 및 FallbackUI 컴포넌트 표시 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import { useEffect } from 'react';
 import Footer from '@/components/layout/Footer';
 import Modal from '@/components/shared/Modal';
 import { useModalStore } from '@/store/useModalStore';
+import { useFallbackStore } from '@/store/useFallbackStore';
+import FallbackUI from '@/components/feedback/FallbackUI';
 
 function App() {
+  const { visible } = useFallbackStore();
   const initTheme = useThemeStore((state) => state.initTheme);
   const { isOpen } = useModalStore();
   useEffect(() => {
@@ -15,8 +18,10 @@ function App() {
 
   return (
     <>
+      {visible && <FallbackUI />}
       <Header />
-      <main className="flex-1 w-full px-content py-10">
+      <main className="flex-1 w-full px-content pb-24 pt-8">
+        {/* React Router 자체적으로 에러를 감싸고 처리 (throw X) -> outlet 위에*/}
         <Outlet />
       </main>
       {isOpen && <Modal />}
