4718a7c3ec013ade31b625164e8cee01eb363d43
SanghyeonLee <hyeon012366@gmail.com>
2025-07-03 22:27:32 +0900

    fix: react-youtube 패키지를 제거하고 iframe으로 YouTube 비디오 표시 방식 변경

--- src/components/shared/YoutubeVideo.tsx
+++ src/components/shared/YoutubeVideo.tsx
@@ -1,19 +1,15 @@
+import { YOUTUBE_IFRAME } from '@/constants/externalUrl';
 import clsx from 'clsx';
-import YouTube from 'react-youtube';
 
 const YoutubeVideo = ({ id, className }: { id: string; className?: string }) => {
   return (
     <div className={clsx(className, 'aspect-video rounded-md overflow-hidden')}>
-      <YouTube
-        videoId={id}
+      <iframe
         className="w-full h-full"
-        opts={{
-          width: '100%',
-          height: '100%',
-          playerVars: {
-            autoplay: 0,
-          },
-        }}
+        src={YOUTUBE_IFRAME(id)}
+        title="YouTube video"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
       />
     </div>
   );
