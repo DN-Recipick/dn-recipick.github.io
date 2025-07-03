5bd35129b8e5e4be20c586ba40eea43779a1f9a4
SanghyeonLee <hyeon012366@gmail.com>
2025-07-03 22:27:59 +0900

    fix: useSignout 훅에서 로그아웃 처리 방식 개선 및 성공 메시지 수정

--- src/features/auth/hooks/useSignout.ts
+++ src/features/auth/hooks/useSignout.ts
@@ -1,23 +1,18 @@
 import { useCallback } from 'react';
-import { useAuthStore } from '@/store/useAuthStore';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '@/constants/routes';
-import { useQueryClient } from '@tanstack/react-query';
 import { showToast } from '@/utils/toast';
 import { SUCCESS_MESSAGES } from '@/constants/messages';
+import { resetAuthState } from './../../../utils/auth';
 
 export const useSignout = () => {
-  const queryClient = useQueryClient();
-  const clearUser = useAuthStore((s) => s.clearUser);
   const navigate = useNavigate();
 
   const signout = useCallback(() => {
-    localStorage.removeItem('token');
-    showToast.success(SUCCESS_MESSAGES.auth.logout);
-    clearUser();
-    queryClient.clear();
+    resetAuthState();
+    showToast.success(SUCCESS_MESSAGES.auth.signout);
     navigate(ROUTES.HOME);
-  }, [clearUser, navigate, queryClient]);
+  }, [navigate]);
 
   return signout;
 };
