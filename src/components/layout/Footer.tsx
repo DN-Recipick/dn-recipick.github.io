b28beafc6a1b5ab9e54605014dad8a6ad623c3a0
SanghyeonLee <hyeon012366@gmail.com>
2025-07-04 01:47:17 +0900

    fix: Footer 컴포넌트에서 DropdownList 제거 및 버튼 내부 텍스트 속성 수정

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,4 @@
 import Dropdown from '@/components/layout/Dropdown/Dropdown';
-import DropdownList from '@/components/layout/Dropdown/DropdownList';
 import DropdownItems from '@/components/layout/Dropdown/RenderDropdownItems';
 import { THEME_OPTIONS } from '@/constants/dropdownOptions';
 import { useThemeStore } from '@/store/useThemeStore';
@@ -7,22 +6,16 @@ import { useThemeStore } from '@/store/useThemeStore';
 const Footer = () => {
   const { theme, setTheme } = useThemeStore();
   return (
-    <footer className="flex-center-between gap-2 w-full p-content">
+    <footer className="flex-center gap-1 w-full p-content">
       <small className="sm:text-[0.7rem] text-[0.6rem]">
         © 2025 Sanghyeon Lee. All rights reserved.
       </small>
       <Dropdown
-        buttonInner={THEME_OPTIONS.find((o) => o.text === theme)?.icon}
-        dropdownList={({ setOpen }) => (
-          <DropdownList className="bottom-8 right-0">
-            <DropdownItems
-              options={THEME_OPTIONS}
-              setOpen={setOpen}
-              onSelect={(option) => setTheme(option.text)}
-            />
-          </DropdownList>
-        )}
-      />
+        dropdownListClassName="bottom-8 right-0"
+        buttonInner={THEME_OPTIONS.find((o) => o.label === theme)?.icon}
+      >
+        <DropdownItems options={THEME_OPTIONS} onSelect={(option) => setTheme(option.label)} />
+      </Dropdown>
     </footer>
   );
 };
