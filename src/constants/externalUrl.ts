fa33af10adb65978a5cae6e3493e60f3a13fd378
SanghyeonLee <hyeon012366@gmail.com>
2025-07-03 22:27:39 +0900

    feat: YOUTUBE_IFRAME 상수 추가하여 YouTube 비디오 iframe URL 생성 기능 구현

--- src/constants/externalUrl.ts
+++ src/constants/externalUrl.ts
@@ -1,3 +1,4 @@
 export const KURLY_ITEM_URL = (no: string) => `https://www.kurly.com/goods/${no}`;
 export const YOUTUBE_THUMBNAIL = (id: string) =>
   `https://img.youtube.com/vi/${id}/maxresdefault.jpg`;
+export const YOUTUBE_IFRAME = (id: string) => `https://www.youtube.com/embed/${id}?autoplay=0`;
