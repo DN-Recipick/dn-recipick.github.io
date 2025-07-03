986be98f18357317cf44f9a6a72a596c1a9e3938
SanghyeonLee <hyeon012366@gmail.com>
2025-07-03 22:29:09 +0900

    feat: QueryCache 추가 및 에러 처리 개선

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,7 +1,11 @@
-import { QueryClient } from '@tanstack/react-query';
+import { QueryCache, QueryClient } from '@tanstack/react-query';
 import { handleAppError } from '@/utils/errorHandler';
 
 export const customQueryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => handleAppError(error),
+    //쿼리 결과 데이터 자체와 상태"를 저장하고 관리하는 저장소
+  }),
   defaultOptions: {
     queries: {
       retry: 1,
