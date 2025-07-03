97d133fc84a7f68a7dce4bc7292f030d45d8c102
SanghyeonLee <hyeon012366@gmail.com>
2025-07-04 01:47:21 +0900

    fix: Header 컴포넌트에서 Logo 컴포넌트 사용 및 DropdownList 제거

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,40 +1,27 @@
 import { ROUTES } from '@/constants/routes';
 import { useNavigate } from 'react-router-dom';
-import Logo from '@/assets/logo.svg';
-import Image from '@/components/shared/Image';
 import { useAuthStore } from '@/store/useAuthStore';
 import Button from '@/components/shared/Button';
 import Dropdown from '@/components/layout/Dropdown/Dropdown';
 import { USER_OPTIONS } from '@/constants/dropdownOptions';
-import { useThemeStore } from '@/store/useThemeStore';
 import { FaUserAlt } from 'react-icons/fa';
-import DropdownList from '@/components/layout/Dropdown/DropdownList';
 import { useSignout } from '@/features/auth/hooks/useSignout';
 import DropdownItems from '@/components/layout/Dropdown/RenderDropdownItems';
+import Logo from './../shared/Logo';
 
 const Header = () => {
-  const { isSignin } = useAuthStore();
+  const { isSignin, user } = useAuthStore();
   const navigate = useNavigate();
   const handleSignout = useSignout();
 
   return (
     <header className="w-full p-content flex-center-between z-30">
-      <Image src={Logo} onClick={() => navigate(ROUTES.HOME)} wrapClassName="w-12 cursor-pointer" />
+      <Logo />
       {isSignin ? (
-        <Dropdown
-          buttonInner={<FaUserAlt />}
-          dropdownList={({ setOpen }) => (
-            <DropdownList className="right-0">
-              <DropdownItems
-                options={USER_OPTIONS}
-                setOpen={setOpen}
-                onSelect={(option) => {
-                  if (option.text === '로그아웃') handleSignout();
-                }}
-              />
-            </DropdownList>
-          )}
-        />
+        <Dropdown dropdownListClassName="right-0" buttonInner={<FaUserAlt />}>
+          <DropdownItems options={[{ text: `${user?.email}님`, label: 'my' }]} />
+          <DropdownItems options={USER_OPTIONS} onSelect={() => handleSignout()} />
+        </Dropdown>
       ) : (
         <div className="flex gap-2">
           <Button onClick={() => navigate(ROUTES.SIGNIN)} className="btn-primary" text="로그인" />
