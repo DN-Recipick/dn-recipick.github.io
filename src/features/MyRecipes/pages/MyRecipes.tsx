ac57cebad64daa22a8650ee82ff0039baee980f9
SanghyeonLee <hyeon012366@gmail.com>
2025-07-03 22:28:43 +0900

    refactor: useGetRecipes 훅을 통해 레시피 데이터 로드 방식 개선 및 UI 구성 수정

--- src/features/MyRecipes/pages/MyRecipes.tsx
+++ src/features/MyRecipes/pages/MyRecipes.tsx
@@ -1,147 +1,23 @@
-import SkeletonRecipeItem from '@/components/feedback/Skeleton/SkeletonRecipeItem';
 import Button from '@/components/shared/Button';
 import { ROUTES } from '@/constants/routes';
 import RecipeList from '@/features/MyRecipes/components/RecipeList';
-import RecipeItem from '@/features/MyRecipes/components/RecipeList';
 import useGetRecipes from '@/features/MyRecipes/hooks/useGetRecipes';
 import { Link } from 'react-router-dom';
-
+import { FaPlus } from 'react-icons/fa6';
 const MyRecipes = () => {
-  const {
-    //data,
-    isPending,
-  } = useGetRecipes();
+  const { data, isPending } = useGetRecipes();
 
-  const data = {
-    recipes: [
-      {
-        id: 'd38b54cd-3c13-4eb0-9ad1-b8101b83a4e9',
-        item: [
-          '1. 김치를 꺼내요',
-          '2. 된장을 꺼내요',
-          '3. 아무튼 물을 넣고 끓여요',
-          '4. 접시에 담아요',
-        ],
-        name: '목데이터 레시피이름',
-        state: 1,
-        title: '목데이터 영상제목',
-        channel: '목데이터 영상채널',
-        video_id: 'yy5IetkEZWI',
-        ingredients: [
-          {
-            name: '김치',
-            amount: '한포기',
-          },
-          {
-            name: '된장',
-            amount: '100g',
-          },
-          {
-            name: '시래기',
-            amount: '한단',
-          },
-        ],
-        saved_at: '2025-06-27T05:17:16.161+00:00',
-      },
-      {
-        id: '39ef9ad2-4511-4c81-b090-7727d571d6b3',
-        item: [
-          '1. 김치를 꺼내요',
-          '2. 된장을 꺼내요',
-          '3. 아무튼 물을 넣고 끓여요',
-          '4. 접시에 담아요',
-        ],
-        name: '목데이터 레시피이름',
-        state: 1,
-        title: '목데이터 영상제목',
-        channel: '목데이터 영상채널',
-        video_id: 'ru5IetkEZWI',
-        ingredients: [
-          {
-            name: '김치',
-            amount: '한포기',
-          },
-          {
-            name: '된장',
-            amount: '100g',
-          },
-          {
-            name: '시래기',
-            amount: '한단',
-          },
-        ],
-        saved_at: '2025-06-27T05:15:53.295+00:00',
-      },
-      {
-        id: '4141a684-4ae2-4127-b43d-c3537bfc7190',
-        item: [
-          '1. 김치를 꺼내요',
-          '2. 된장을 꺼내요',
-          '3. 아무튼 물을 넣고 끓여요',
-          '4. 접시에 담아요',
-        ],
-        name: '목데이터 레시피이름',
-        state: 1,
-        title: '목데이터 영상제목',
-        channel: '목데이터 영상채널',
-        video_id: '5eIJEBOFTog',
-        ingredients: [
-          {
-            name: '김치',
-            amount: '한포기',
-          },
-          {
-            name: '된장',
-            amount: '100g',
-          },
-          {
-            name: '시래기',
-            amount: '한단',
-          },
-        ],
-        saved_at: '2025-06-27T05:13:40.849+00:00',
-      },
-      {
-        id: 'db8deb72-27b6-4a30-ae2f-a913538b5d8a',
-        item: [
-          '1. 김치를 꺼내요',
-          '2. 된장을 꺼내요',
-          '3. 아무튼 물을 넣고 끓여요',
-          '4. 접시에 담아요',
-        ],
-        name: '목데이터 레시피이름',
-        state: 1,
-        title: '목데이터 영상제목',
-        channel: '목데이터 영상채널',
-        video_id: 'K1GBUzbsfNM',
-        ingredients: [
-          {
-            name: '김치',
-            amount: '한포기',
-          },
-          {
-            name: '된장',
-            amount: '100g',
-          },
-          {
-            name: '시래기',
-            amount: '한단',
-          },
-        ],
-        saved_at: '2025-06-27T04:25:48.182+00:00',
-      },
-    ],
-    count: 4,
-  };
   return (
     <>
-      <div className="flex justify-between items-start gap-5">
-        <h2>내 레시피</h2>
+      <h2>내 레시피</h2>
+      <RecipeList isPending={isPending} recipes={data?.recipes} />
+      <div className="fixed bottom-23 w-full layout-max-w px-4 left-1/2 -translate-x-1/2 z-50 flex justify-end">
         <Link to={ROUTES.HOME}>
-          <Button className="btn-primary" text="레시피 추가" />
+          <Button className="btn-primary w-14 h-14 rounded-full opacity-80 hover:opacity-100 transition-opacity duration-200">
+            <FaPlus />
+          </Button>
         </Link>
       </div>
-      <RecipeList isPending={isPending} recipes={data?.recipes} />
     </>
   );
 };
