6e3ccd2e853e029f2069c723f8182eb389373dd7
SanghyeonLee <hyeon012366@gmail.com>
2025-07-04 01:48:14 +0900

    feat: RecipeItem 컴포넌트 추가 및 기본 스타일 설정

---
+++ src/features/MyRecipes/components/RecipeItem.tsx
@@ -0,0 +1,33 @@
+import Image from '@/components/shared/Image';
+import { YOUTUBE_THUMBNAIL } from '@/constants/externalUrl';
+import { ROUTES } from '@/constants/routes';
+import type { Recipe } from '@/types/recipe';
+import { formatDateTime } from '@/utils/format';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const RecipeItem = ({ recipe }: { recipe: Recipe }) => {
+  const navigate = useNavigate();
+  return (
+    <li className="h-full" key={recipe.id}>
+      <article
+        className="recipe-item-wrap transition-transform duration-200 hover:scale-[1.02] hover:shadow-lg cursor-pointer bg-white dark:bg-gray-800"
+        onClick={() => navigate(ROUTES.RECIPE(recipe.id))}
+      >
+        <Image src={YOUTUBE_THUMBNAIL(recipe.video_id)} alt={recipe.title} wrapClassName="h-45" />
+        <div className="p-3 flex-1 flex-column justify-between">
+          <h3 className="text-lg font-extrabold line-clamp-2">{recipe.name}</h3>
+          <div className="flex-column gap-2">
+            <p className="text font-bold line-clamp-2">{recipe.title}</p>
+            <p className="text-sm font-bold text-gray-400">{recipe.channel}</p>
+            <p className="text-sm text-gray-400 font-sans font-bold">
+              {formatDateTime(recipe.saved_at)}
+            </p>
+          </div>
+        </div>
+      </article>
+    </li>
+  );
+};
+
+export default React.memo(RecipeItem);
