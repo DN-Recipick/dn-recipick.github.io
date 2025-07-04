df42888ed916e7108abbaf7b2828ef8ea80104ac
SanghyeonLee <hyeon012366@gmail.com>
2025-07-04 23:53:37 +0900

    feat: RECIPE 쿼리 키에 추천 레시피 추가

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -8,6 +8,7 @@ export const queryKeys = {
   },
   RECIPE: {
     all: ['recipes'],
+    recommended: (id: string) => ['recommendedRecipes', id],
     item: (id: string) => ['recipes', id],
   },
   KURLY_ITEM: (keyword: string) => ['kurly', keyword],
