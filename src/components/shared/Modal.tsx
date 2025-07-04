d363e1ffc0b0101f01ac52e7fc589809a6010f83
SanghyeonLee <hyeon012366@gmail.com>
2025-07-04 23:53:21 +0900

    fix: Modal 컴포넌트에서 최대 너비를 2xl로 변경하고 RecommendedRecipes 추가

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -4,6 +4,7 @@ import Button from '@/components/shared/Button';
 import { HiX } from 'react-icons/hi';
 import { useModalEffect } from '@/components/shared/hook/useModalEffect';
 import IngredientDetail from '@/features/MyRecipeDetail/components/IngredientDetail';
+import RecommendedRecipes from '@/features/MyRecipeDetail/components/RecommendedRecipes';
 
 const Modal = () => {
   const { data, type, isOpen, close } = useModalStore();
@@ -14,21 +15,24 @@ const Modal = () => {
   return createPortal(
     <div className="fixed inset-0 z-50 flex-center bg-black/50" onClick={close}>
       <div
-        className="bg-white dark:bg-gray-800 rounded-md shadow-xl max-w-xl w-full min-h-[18.75rem] max-h-[80vh] 
+        className="bg-white dark:bg-gray-800 rounded-md shadow-xl max-w-2xl w-full min-h-[18.75rem] max-h-[80vh] 
               transition-all duration-300 transform -translate-y-5 opacity-0
               animate-fade-in-up relative"
         onClick={(e) => e.stopPropagation()}
       >
         <div className="sticky top-0 p-3 shadow-[var(--shadow)]">
           <h3 className="text-center h-15 content-center mb-0">{data?.title}</h3>
-          <Button onClick={close} className="btn-icon text-gray-500 absolute right-2 top-2">
-            <HiX className="text-2xl" />
-          </Button>
+          <Button
+            onClick={close}
+            icon={<HiX className="text-3xl" />}
+            className="btn-icon text-gray-500 absolute right-2 top-2"
+          />
         </div>
         <div className="max-h-[65vh] overflow-y-auto px-content">
           {type === 'ingredient' && (
             <IngredientDetail ingredientKeyword={data?.ingredientKeyword as string} />
           )}
+          {type === 'relatedRecipes' && <RecommendedRecipes id={data?.id as string} />}
         </div>
       </div>
     </div>,
