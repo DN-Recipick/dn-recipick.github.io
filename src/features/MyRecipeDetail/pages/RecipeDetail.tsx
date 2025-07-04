530daf18df6c8cb756dac577818eb08b291fe543
SanghyeonLee <hyeon012366@gmail.com>
2025-07-04 23:57:39 +0900

    refactor: pageLayout 추가 및 버튼 구조 수정

--- src/features/MyRecipeDetail/pages/RecipeDetail.tsx
+++ src/features/MyRecipeDetail/pages/RecipeDetail.tsx
@@ -1,4 +1,4 @@
-import FullScreenLoader from '@/components/feedback/FullScreenLoader';
+import FullScreenLoader from '@/components/feedback/loading/FullScreenLoader';
 import { ROUTES } from '@/constants/routes';
 import Ingredients from '@/features/MyRecipeDetail/components/Ingredients';
 import RecipeProcedure from '@/features/MyRecipeDetail/components/RecipeProcedure';
@@ -6,23 +6,31 @@ import useGetRecipeItem from '@/features/MyRecipeDetail/hooks/useGetRecipeItem';
 import { Navigate, useParams } from 'react-router-dom';
 import { formatDateTime } from '@/utils/format';
 import YoutubeVideo from '@/components/shared/YoutubeVideo';
+import Button from '@/components/shared/Button';
+import { useModalStore } from '@/store/useModalStore';
+import PageLayout from '@/components/shared/PageLayout';
 
 const RecipeDetail = () => {
+  const { open } = useModalStore();
   const { id } = useParams();
   const { recipeItem, isPending } = useGetRecipeItem(id ?? '');
 
   if (!id) return <Navigate to={ROUTES.NOT_FOUND} />;
   if (isPending || !recipeItem) return <FullScreenLoader />;
   return (
-    <div>
-      <h2>{recipeItem?.name}</h2>
+    <PageLayout title={recipeItem?.name}>
       <p>{formatDateTime(recipeItem?.created_at)}</p>
       <YoutubeVideo id={recipeItem.video_id} />
       <h3 className="mt-5">{recipeItem?.title}</h3>
       <p>{recipeItem?.channel}</p>
       <RecipeProcedure procedure={recipeItem?.item} />
       <Ingredients ingredients={recipeItem?.ingredients} />
-    </div>
+      <Button
+        text="추천 레시피"
+        className="btn-primary"
+        onClick={() => open('relatedRecipes', { title: '추천 레시피', id: recipeItem.id })}
+      />
+    </PageLayout>
   );
 };
 
