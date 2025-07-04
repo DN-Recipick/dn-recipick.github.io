ffa5ad4ebceeb2d04845f906028af9948baf2bb2
SanghyeonLee <hyeon012366@gmail.com>
2025-07-04 23:53:18 +0900

    fix: Logo 컴포넌트의 이미지 크기 및 텍스트 스타일 수정

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -6,10 +6,11 @@ import LogoImg from '@/assets/logo.svg';
 const Logo = () => {
   return (
     <Link to={ROUTES.HOME} className="flex-center gap-2 rounded-xl hover-opacity">
-      <Image src={LogoImg} alt={LogoImg} wrapClassName="w-12 cursor-pointer" />
+      <Image src={LogoImg} alt={LogoImg} wrapClassName="w-11 sm:w-12 cursor-pointer" />
       <p
         className="
-    text-xl font-extrabold text-transparent bg-clip-text 
+        text-[1.1rem]
+    sm:text-xl font-extrabold text-transparent bg-clip-text 
     bg-gradient-to-r 
     from-[#18644f] via-[#25947a] to-[#36b49a]
     dark:from-[#268f72] dark:via-[#43b99d] dark:to-[#a7f3d0]
