a7783423283967973d96a53880a8e0e147198cec
SanghyeonLee <hyeon012366@gmail.com>
2025-07-05 00:00:34 +0900

    feat: 모달 타입 추가

--- src/store/useModalStore.ts
+++ src/store/useModalStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 
-export type ModalType = 'ingredient' | null;
+export type ModalType = 'ingredient' | 'relatedRecipes' | null;
 
 interface ModalData {
   title: string;
