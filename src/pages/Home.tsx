94302bd2a0c202ea99be669f9f40225b6bf19bc1
SanghyeonLee <hyeon012366@gmail.com>
2025-07-05 00:00:23 +0900

    design: break-keep 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="full-page-center">
       <div className="max-w-xl w-full">
-        <h2>영상으로 쉽게 레시피를 알아보세요</h2>
+        <h2 className="break-keep">영상으로 쉽게 레시피를 알아보세요</h2>
         <RecipeLinkForm />
         <Link to={ROUTES.RECIPES}>
           <Button className="btn-primary w-full" text="내 레시피" />
