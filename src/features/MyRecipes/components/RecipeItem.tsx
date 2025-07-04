e026e9f89edaf77e4223ae2f2c29397b90ccb7ce
SanghyeonLee <hyeon012366@gmail.com>
2025-07-04 23:57:59 +0900

    refactor: 불필요 article 제거

--- src/features/MyRecipes/components/RecipeItem.tsx
+++ src/features/MyRecipes/components/RecipeItem.tsx
@@ -9,23 +9,21 @@ import { useNavigate } from 'react-router-dom';
 const RecipeItem = ({ recipe }: { recipe: Recipe }) => {
   const navigate = useNavigate();
   return (
-    <li className="h-full" key={recipe.id}>
-      <article
-        className="recipe-item-wrap transition-transform duration-200 hover:scale-[1.02] hover:shadow-lg cursor-pointer bg-white dark:bg-gray-800"
-        onClick={() => navigate(ROUTES.RECIPE(recipe.id))}
-      >
-        <Image src={YOUTUBE_THUMBNAIL(recipe.video_id)} alt={recipe.title} wrapClassName="h-45" />
-        <div className="p-3 flex-1 flex-column justify-between">
-          <h3 className="text-lg font-extrabold line-clamp-2">{recipe.name}</h3>
-          <div className="flex-column gap-2">
-            <p className="text font-bold line-clamp-2">{recipe.title}</p>
-            <p className="text-sm font-bold text-gray-400">{recipe.channel}</p>
-            <p className="text-sm text-gray-400 font-sans font-bold">
-              {formatDateTime(recipe.saved_at)}
-            </p>
-          </div>
+    <li
+      onClick={() => navigate(ROUTES.RECIPE(recipe.id))}
+      className="recipe-item-wrap transition-transform duration-200 hover:scale-[1.02] hover:shadow-lg cursor-pointer bg-[var(--color-list-item-bg)]"
+    >
+      <Image src={YOUTUBE_THUMBNAIL(recipe.video_id)} alt={recipe.title} wrapClassName="h-45" />
+      <div className="p-3 flex-1 flex-column justify-between">
+        <h3 className="text-lg font-extrabold line-clamp-2">{recipe.name}</h3>
+        <div className="flex-column gap-2">
+          <p className="font-bold line-clamp-2">{recipe.title}</p>
+          <p className="text-sm font-bold text-gray-400 line-clamp-1">{recipe.channel}</p>
+          <p className="text-sm text-gray-400 font-sans font-bold">
+            {formatDateTime(recipe.saved_at ?? recipe.created_at)}
+          </p>
         </div>
-      </article>
+      </div>
     </li>
   );
 };
