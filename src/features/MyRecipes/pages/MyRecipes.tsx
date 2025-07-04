fbf93f9d522d6641fd6668ca29c57a0b9e6cc03f
SanghyeonLee <hyeon012366@gmail.com>
2025-07-04 23:59:30 +0900

    feat: 페이지 레이아웃 래핑

--- src/features/MyRecipes/pages/MyRecipes.tsx
+++ src/features/MyRecipes/pages/MyRecipes.tsx
@@ -4,21 +4,22 @@ import RecipeList from '@/features/MyRecipes/components/RecipeList';
 import useGetRecipes from '@/features/MyRecipes/hooks/useGetRecipes';
 import { Link } from 'react-router-dom';
 import { FaPlus } from 'react-icons/fa6';
+import PageLayout from '@/components/shared/PageLayout';
 const MyRecipes = () => {
   const { data, isPending } = useGetRecipes();
 
   return (
-    <>
-      <h2>내 레시피</h2>
+    <PageLayout title="내 레시피">
       <RecipeList isPending={isPending} recipes={data?.recipes} />
       <div className="fixed bottom-23 w-full layout-max-w px-content left-1/2 -translate-x-1/2 z-10 flex justify-end">
         <Link to={ROUTES.HOME}>
-          <Button className="btn-primary w-14 h-14 rounded-full opacity-90 hover:opacity-100 transition-opacity duration-200">
-            <FaPlus />
-          </Button>
+          <Button
+            icon={<FaPlus className="mini-icon-size" />}
+            className="btn-primary w-14 h-14 rounded-full opacity-90 hover:opacity-100 transition-opacity duration-200"
+          />
         </Link>
       </div>
-    </>
+    </PageLayout>
   );
 };
 
