48a4c8ad0554c2109c4ce6fc4c46a880c4010c67
SanghyeonLee <hyeon012366@gmail.com>
2025-07-04 23:53:43 +0900

    feat: SHARE_RECIPE 경로 추가

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -4,6 +4,7 @@ export const ROUTES = {
   SIGNUP: '/signup',
   RECIPES: '/recipes',
   RECIPE: (id: string) => `/recipes/${id}`,
+  SHARE_RECIPE: '/add',
   NOT_FOUND: '/404',
   RECIPE_PATH: '/recipes/:id',
   ALL: '*',
