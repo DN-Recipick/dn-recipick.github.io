0bb42c1ff71f90f7fb6d542fbdbe1498610dbd45
SanghyeonLee <hyeon012366@gmail.com>
2025-07-09 05:32:49 +0900

    refactor: 불필요 코드 제거 및 수파베이스 클라이언트 사용 로직으로 추가

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,16 +1,19 @@
-import { customQueryClient } from '@/lib/queryClient';
-import { useAuthStore } from '@/store/useAuthStore';
+import { supabaseClient } from '@/lib/supabaseClient';
+
 const API_SUPABASE_KEY = import.meta.env.VITE_API_SUPABASE_KEY;
 
-export const getAuthHeaders = () => ({
-  Authorization: `Bearer ${localStorage.getItem('token') ?? ''}`,
-});
+export const getAuthHeaders = async () => {
+  const {
+    data: { session },
+  } = await supabaseClient.auth.getSession();
+
+  const accessToken = session?.access_token ?? '';
+
+  return {
+    Authorization: `Bearer ${accessToken}`,
+  };
+};
 
 export const getSupabaseApiKey = () => ({
   apikey: API_SUPABASE_KEY,
 });
-export const resetAuthState = () => {
-  localStorage.removeItem('token');
-  useAuthStore.getState().clearUser();
-  customQueryClient.clear();
-};
