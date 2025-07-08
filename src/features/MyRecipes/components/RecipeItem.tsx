955d58f7520a3dc6f55c105a4d3d1a78f2140f88
SanghyeonLee <hyeon012366@gmail.com>
2025-07-09 05:25:25 +0900

    feat: 타인의 레시피에서 넘어갔을때 모달 닫는 기능 및 ui 수정

--- src/features/MyRecipes/components/RecipeItem.tsx
+++ src/features/MyRecipes/components/RecipeItem.tsx
@@ -5,19 +5,24 @@ import { formatDateTime } from '@/utils/format';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { YOUTUBE } from '@/constants/externalUrl';
+import { useModalStore } from '@/store/useModalStore';
 
 const RecipeItem = ({ recipe }: { recipe: Recipe }) => {
   const navigate = useNavigate();
+  const { isOpen, close } = useModalStore();
   return (
     <li
-      onClick={() => navigate(ROUTES.RECIPE(recipe.id))}
+      onClick={() => {
+        if (isOpen) close();
+        navigate(ROUTES.RECIPE(recipe.id));
+      }}
       className="recipe-item-wrap border border-[var(--color-border)] transition-transform duration-200 hover:scale-[1.02] hover:shadow-lg cursor-pointer bg-[var(--color-card-bg)]"
     >
       <Image src={YOUTUBE.THUMBNAIL(recipe.video_id)} alt={recipe.title} wrapClassName="h-45" />
-      <div className="p-3 flex-1 flex-column justify-between">
-        <h3 className="text-lg font-extrabold line-clamp-2">{recipe.name}</h3>
-        <div className="flex-column gap-2">
-          <p className="font-bold line-clamp-2">{recipe.title}</p>
+      <h3 className="text-lg pt-3 px-3 font-extrabold line-clamp-1">{recipe.name}</h3>
+      <div className="p-3 flex-column flex-1 justify-between">
+        <p className="font-bold line-clamp-2">{recipe.title}</p>
+        <div className="flex-column gap-2 mt-2">
           <p className="text-sm font-bold text-[var(--color-sub-text)] line-clamp-1">
             {recipe.channel}
           </p>
