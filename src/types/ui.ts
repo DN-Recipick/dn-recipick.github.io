fd622b364d2ed297e264d924d15c40ebe781b00b
SanghyeonLee <hyeon012366@gmail.com>
2025-07-09 05:32:21 +0900

    refactor: 타입 코드 분리

--- src/types/ui.ts
+++ src/types/ui.ts
@@ -1,1 +1,7 @@
+import type { RecipeLink } from '@/validation/recipeLink.schema';
+
 export type Theme = 'light' | 'dark' | 'system';
+
+export type FocusableField = keyof RecipeLink;
+
+export type ModalType = 'ingredient' | 'recommendRecipes' | null;
