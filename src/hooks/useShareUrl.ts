a85dff2b8b193bbb15930631adba1864028ad03e
SanghyeonLee <hyeon012366@gmail.com>
2025-07-09 05:28:20 +0900

    feat: 공유 했을때 추가되는 커스텀 훅 추가

--- src/hooks/useShareUrl.ts
+++ src/hooks/useShareUrl.ts
@@ -1,14 +1,21 @@
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { addRecipe } from '@/features/RecipeLink/api';
+import { ROUTES } from '@/constants/routes';
+import { queryKeys } from '@/constants/queryKeys';
 
 export const useShareUrl = () => {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
   const sharedUrl = searchParams.get('url');
-
+  const navigate = useNavigate();
   const { mutate } = useMutation({
     mutationFn: (url: string) => addRecipe({ url }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: queryKeys.RECIPE.all });
+      navigate(ROUTES.RECIPES);
+    },
   });
 
   useEffect(() => {
