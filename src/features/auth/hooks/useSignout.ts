0b9363ec21d0326f2f3552c0e7bb6ac21ce457ce
SanghyeonLee <hyeon012366@gmail.com>
2025-07-09 05:20:34 +0900

    feat: 수파베이스 클라리언트 사용으로 인한 로그아웃 코드 재 정립

--- src/features/auth/hooks/useSignout.ts
+++ src/features/auth/hooks/useSignout.ts
@@ -3,16 +3,25 @@ import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '@/constants/routes';
 import { showToast } from '@/utils/toast';
 import { SUCCESS_MESSAGES } from '@/constants/messages';
-import { resetAuthState } from './../../../utils/auth';
-
+import { supabaseClient } from '@/lib/supabaseClient';
+import { CustomError } from '@/lib/CustomError';
+import { useQueryClient } from '@tanstack/react-query';
 export const useSignout = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
-  const signout = useCallback(() => {
-    resetAuthState();
+  const signout = useCallback(async () => {
+    const { error } = await supabaseClient.auth.signOut();
+
+    if (error) {
+      throw new CustomError(error.status ?? 400, error.message);
+    }
+
+    queryClient.clear();
+
     showToast.success(SUCCESS_MESSAGES.auth.signout);
     navigate(ROUTES.HOME);
-  }, [navigate]);
+  }, [navigate, queryClient]);
 
   return signout;
 };
