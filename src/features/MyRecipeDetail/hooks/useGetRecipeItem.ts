d346c4272fb57117088be22bee129f75ccab95ef
SanghyeonLee <hyeon012366@gmail.com>
2025-07-09 05:24:04 +0900

    refactor: 로딩 상태일때 이름 중복 막기 위한 네이밍 변경

--- src/features/MyRecipeDetail/hooks/useGetRecipeItem.ts
+++ src/features/MyRecipeDetail/hooks/useGetRecipeItem.ts
@@ -3,12 +3,12 @@ import { getRecipeItem } from '@/features/MyRecipeDetail/api';
 import { useQuery } from '@tanstack/react-query';
 
 const useGetRecipeItem = (id: string) => {
-  const { data: recipeItem, isPending } = useQuery({
+  const { data: recipeItem, isPending: isPendingRecipeItem } = useQuery({
     queryKey: queryKeys.RECIPE.item(id),
     queryFn: () => getRecipeItem(id),
     enabled: !!id,
   });
 
-  return { recipeItem, isPending };
+  return { recipeItem, isPendingRecipeItem };
 };
 export default useGetRecipeItem;
