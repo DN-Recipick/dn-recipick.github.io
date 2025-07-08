00e4262f07fca26f059dd504317d17929f52a2b5
SanghyeonLee <hyeon012366@gmail.com>
2025-07-09 05:20:00 +0900

    refactor: 수파베이스 클라이언트 사용으로 인한 불필요 코드 제거

--- src/features/auth/hooks/useSignIn.ts
+++ src/features/auth/hooks/useSignIn.ts
@@ -1,8 +1,7 @@
 import { SUCCESS_MESSAGES } from '@/constants/messages';
 import { ROUTES } from '@/constants/routes';
 import { signin } from '@/features/auth/apis';
-import { useAuthStore } from '@/store/useAuthStore';
-import type { SigninResponse, SigninFormType } from '@/types/auth';
+import type { SigninFormType } from '@/types/auth';
 import { showToast } from '@/utils/toast';
 import { signinSchema } from '@/validation/auth.schema';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -12,7 +11,6 @@ import { useNavigate } from 'react-router-dom';
 
 export const useSignin = () => {
   const queryClient = useQueryClient();
-  const { setUser } = useAuthStore();
   const navigate = useNavigate();
   const {
     register,
@@ -29,9 +27,7 @@ export const useSignin = () => {
 
   const { isPending, mutate } = useMutation({
     mutationFn: (formData: SigninFormType) => signin(formData),
-    onSuccess: (res: SigninResponse) => {
-      localStorage.setItem('token', res?.access_token);
-      setUser(res.user);
+    onSuccess: () => {
       queryClient.clear();
       showToast.success(SUCCESS_MESSAGES.auth.signin);
       navigate(ROUTES.HOME);
