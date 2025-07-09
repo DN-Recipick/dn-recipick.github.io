c1da118ceea08f8ddb720844dfe8b7649dc71896
SanghyeonLee <hyeon012366@gmail.com>
2025-07-09 19:58:36 +0900

    refactor: 에러 상태에 따른 조건부 분기 해서 에러 관리 명확하게 수정

--- src/components/feedback/fallback/FullPageFallback.tsx
+++ src/components/feedback/fallback/FullPageFallback.tsx
@@ -3,24 +3,25 @@ import { useFallbackStore } from '@/store/useFallbackStore';
 import Button from '@/components/shared/Button';
 import { ROUTES } from '@/constants/routes';
 import { CustomError } from '@/lib/CustomError';
+import { useNavigate } from 'react-router-dom';
 //렌더링 오류는 상태로 관리하지 않는다
 const FullPageFallback = (props?: Partial<FallbackProps>) => {
-  const { visible, message, rawMessage, status } = useFallbackStore();
+  const { visible, message, rawMessage, status, reset } = useFallbackStore();
+  const navigate = useNavigate();
 
   const navigatePage = () => {
+    reset();
     props?.resetErrorBoundary?.(); // ErrorBoundary 상태 초기화
-    window.location.href = ROUTES.HOME;
+    navigate(ROUTES.HOME);
   };
 
-  const isRenderingError = !(props?.error instanceof CustomError);
-
-  if (!visible && !isRenderingError) return null;
+  if (!visible) return null;
 
   return (
     <div className="full-page-center">
       <h1>{status ?? 500}</h1>
-      <p>{isRenderingError ? '렌더링 에러 발생' : message}</p>
-      <p className="text-[var(--color-danger)]">{rawMessage}</p>
+      <p>{props?.error instanceof CustomError ? message : '렌더링 에러 발생'}</p>
+      <p className="text-[var(--color-danger)] break-all">{rawMessage}</p>
       <Button text="홈으로 돌아가기" onClick={navigatePage} className="btn-primary" />
     </div>
   );
