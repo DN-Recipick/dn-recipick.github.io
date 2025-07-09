60fb9a3364719ab96d646649f1a167145b3e7581
SanghyeonLee <hyeon012366@gmail.com>
2025-07-10 02:35:41 +0900

    feat: 수파베이스 클라이언트 통한 레시피 실시간 로딩상태 구현

--- src/features/MyRecipes/hooks/useGetRecipes.ts
+++ src/features/MyRecipes/hooks/useGetRecipes.ts
@@ -1,12 +1,49 @@
 import { queryKeys } from '@/constants/queryKeys';
 import { getRecipes } from '@/features/MyRecipes/api';
-import { useQuery } from '@tanstack/react-query';
+import { supabaseClient } from '@/lib/supabaseClient';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
+
+const QUERY_KEY_RECIPES = queryKeys.RECIPE.all;
 
 const useGetRecipes = () => {
+  const queryClient = useQueryClient();
+
   const { data, isPending } = useQuery({
-    queryKey: queryKeys.RECIPE.all,
+    queryKey: QUERY_KEY_RECIPES,
     queryFn: getRecipes,
   });
-  return { data, isPending };
+
+  useEffect(() => {
+    const channel = supabaseClient.channel('recipe-state');
+
+    // UPDATE 이벤트에서 state가 1이 될 때만 쿼리 무효화
+    channel
+      .on(
+        'postgres_changes',
+        {
+          event: 'UPDATE',
+          schema: 'public',
+          table: 'recipe',
+        },
+        (payload) => {
+          console.log('레시피 업데이트됨:', payload.new);
+          if (payload.new.state === 1) {
+            queryClient.invalidateQueries({ queryKey: QUERY_KEY_RECIPES });
+          }
+        },
+      )
+      .subscribe();
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, [queryClient]);
+
+  return {
+    recipes: data?.recipes,
+    isPending,
+  };
 };
+
 export default useGetRecipes;
