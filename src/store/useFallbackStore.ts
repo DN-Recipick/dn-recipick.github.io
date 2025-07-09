bf494dfc984236b267b9bdc2d9222931e8f9c471
SanghyeonLee <hyeon012366@gmail.com>
2025-07-09 20:01:47 +0900

    feat: 에러 상태 초기화 위한 reset 추가

--- src/store/useFallbackStore.ts
+++ src/store/useFallbackStore.ts
@@ -7,10 +7,12 @@ export const useFallbackStore = create<{
   rawMessage?: string;
   status?: ErrorStatusCode;
   show: (message: string, rawMessage?: string, status?: ErrorStatusCode) => void;
+  reset: () => void;
 }>((set) => ({
   visible: false,
   message: '',
   rawMessage: undefined,
   status: undefined,
   show: (message, rawMessage, status) => set({ visible: true, message, rawMessage, status }),
+  reset: () => set({ visible: false, message: '', rawMessage: undefined, status: undefined }),
 }));
