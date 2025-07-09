9826128f47e5679f14c93123fa8d36925cc0913d
SanghyeonLee <hyeon012366@gmail.com>
2025-07-09 20:01:00 +0900

    refactor: 로그인, 회원가입에서 인증 안된 사용자일시 홈으로 리다렉트 코드 추가

--- src/features/auth/pages/Signin.tsx
+++ src/features/auth/pages/Signin.tsx
@@ -1,7 +1,18 @@
 import PageLayout from '@/components/shared/PageLayout';
+import { ROUTES } from '@/constants/routes';
 import SigninForm from '@/features/auth/components/SigninForm';
+import useGetUser from '@/features/auth/hooks/useGetUser';
+import { Navigate } from 'react-router-dom';
 
 const Signin = () => {
+  const { isSignedIn, isPending } = useGetUser();
+
+  if (isPending) return null;
+
+  if (isSignedIn) {
+    return <Navigate to={ROUTES.HOME} replace />;
+  }
+
   return (
     <PageLayout title="로그인">
       <SigninForm />
--- src/features/auth/pages/Signup.tsx
+++ src/features/auth/pages/Signup.tsx
@@ -1,7 +1,18 @@
 import PageLayout from '@/components/shared/PageLayout';
 import SignupForm from '@/features/auth/components/SignupForm';
+import useGetUser from '@/features/auth/hooks/useGetUser';
+import { Navigate } from 'react-router-dom';
+import { ROUTES } from './../../../constants/routes';
 
 const Signup = () => {
+  const { isSignedIn, isPending } = useGetUser();
+
+  if (isPending) return null;
+
+  if (isSignedIn) {
+    return <Navigate to={ROUTES.HOME} replace />;
+  }
+
   return (
     <PageLayout title="회원가입">
       <SignupForm />
