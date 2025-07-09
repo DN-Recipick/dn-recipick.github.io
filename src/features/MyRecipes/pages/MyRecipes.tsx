f840b542f0d808c7a924b67111609a57a33d1174
SanghyeonLee <hyeon012366@gmail.com>
2025-07-10 02:36:18 +0900

    refactor: 가독성 위한 실시간 통신 부분 로직 분리

--- src/features/MyRecipes/pages/MyRecipes.tsx
+++ src/features/MyRecipes/pages/MyRecipes.tsx
@@ -5,59 +5,9 @@ import useGetRecipes from '@/features/MyRecipes/hooks/useGetRecipes';
 import { Link } from 'react-router-dom';
 import { FaPlus } from 'react-icons/fa6';
 import PageLayout from '@/components/shared/PageLayout';
-import { useEffect, useState } from 'react';
-import { supabaseClient } from '@/lib/supabaseClient';
 
 const MyRecipes = () => {
-  const { data, isPending } = useGetRecipes();
-
-  const [recipes, setRecipes] = useState(data?.recipes);
-
-  const recipeStateCheck = (table:string, id:string) => {
-    console.log(`실시간 통신 시작`);
-    
-    const channel = supabaseClient
-    .channel('recipe-state')
-    .on(
-      'postgres_changes',
-      {
-        event: 'UPDATE',
-        schema: 'public',
-        table: table,
-        filter: `id=eq.${id}`,
-      },
-      (payload) => {
-        console.log(payload)
-        if (payload.new.state === 1){
-          setRecipes(recipes.map((recipe) => recipe.id === id ? payload.new : recipe));
-          channel.unsubscribe();
-        }
-      }
-    )
-    .subscribe();
-  }
-
-  useEffect(() => {
-    setRecipes(data?.recipes);
-  }, [isPending]);
-
-  useEffect(() => {
-    if(data){
-      console.log("데이터 들어옴옴")
-
-      console.log(`data:`, data);
-      console.log(`recipes:`,recipes);
-
-      const unprocessedRecipe = recipes?.find((recipe) => recipe.state === 0);
-      console.log(`0인 레시피:`, unprocessedRecipe);
-      if (unprocessedRecipe) {
-        recipeStateCheck('recipe', unprocessedRecipe.id)
-      }
-    }
-
-  }, [recipes]);
-
-
+  const { recipes, isPending } = useGetRecipes();
 
   return (
     <PageLayout title="내 레시피">
