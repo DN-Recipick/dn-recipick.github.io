8cdc30c3c664dfdee471acddf7c40735456000ab
SanghyeonLee <hyeon012366@gmail.com>
2025-07-11 03:19:54 +0900

    feat: 재료 상세 ux 고려한 검색 결과 메세지 추가 및 세부 스타일 조절

--- src/features/MyRecipeDetail/components/IngredientDetail.tsx
+++ src/features/MyRecipeDetail/components/IngredientDetail.tsx
@@ -1,5 +1,5 @@
 import EmptyState from '@/components/feedback/empty/EmptyState';
-import FullScreenLoader from '@/components/feedback/loading/FullScreenLoader';
+import SkeletonIngredientDetail from '@/components/feedback/skeleton/SkeletonIndredientDetail';
 import ALink from '@/components/shared/ALink';
 import Image from '@/components/shared/Image';
 import { KURLY_ITEM_URL } from '@/constants/externalUrl';
@@ -8,31 +8,42 @@ import { formatNumber } from '@/utils/format';
 
 const IngredientDetail = ({ ingredientKeyword }: { ingredientKeyword: string }) => {
   const { isPending, kurlyItems } = useGetKurlyItem(ingredientKeyword);
-  if (isPending) return <FullScreenLoader />;
+  if (isPending) return <SkeletonIngredientDetail />;
   return (
-    <ul className="flex-column divide-y divide-gray-300">
-      {kurlyItems?.length === 0 && <EmptyState text="일치하는 재료링크가 없습니다" />}
-      {kurlyItems?.map((kurlyItem) => (
-        <li key={kurlyItem.no} className="py-content">
-          <article className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4">
-            <div className="flex gap-4.5">
-              <Image src={kurlyItem.imageUrl} wrapClassName="w-30 h-30 rounded-md shrink-0" />
-              <div className="flex flex-col justify-center gap-1 sm:gap-2">
-                <p className="text-xl">{kurlyItem.name}</p>
-                <p>{formatNumber(kurlyItem.price)}원</p>
-              </div>
-            </div>
-            <ALink
-              className="btn-kurly text-center hover-opacity"
-              href={KURLY_ITEM_URL(kurlyItem.no)}
-              text="구매"
-              target="_blank"
-              rel="noopener noreferrer"
+    <>
+      <div className="flex mb-3">
+        <p className="text-[1.1rem] sm:text-xl">컬리에서</p>
+        <p className="text-[var(--color-kurly)] text-[1.1rem] sm:text-xl">'{ingredientKeyword}'</p>
+        <p className="sm:text-xl text-[1.1rem] ">을(를) 검색한 결과입니다.</p>
+      </div>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+        {kurlyItems?.length === 0 && <EmptyState text="일치하는 재료링크가 없습니다" />}
+        {kurlyItems?.map((kurlyItem) => (
+          <li
+            key={kurlyItem.no}
+            className="rounded-xl overflow-hidden shadow-[var(--shadow)] bg-[var(--color-card-bg)] p-3 flex sm:flex-col sm:items-center sm:gap-3 gap-4"
+          >
+            <Image
+              src={kurlyItem.imageUrl}
+              wrapClassName="w-26 h-26 rounded-md shrink-0 sm:w-full sm:h-65 aspect-square object-cover"
             />
-          </article>
-        </li>
-      ))}
-    </ul>
+            <div className="flex flex-col justify-between flex-1 w-full sm:items-center sm:text-center">
+              <p className="text-lg sm:text-base font-medium line-clamp-1">{kurlyItem.name}</p>
+              <p className="text-[var(--color-sub-text)] font-semibold mt-1 mb-2">
+                {formatNumber(kurlyItem.price)}원
+              </p>
+              <ALink
+                className="btn-kurly text-center hover-opacity w-full"
+                href={KURLY_ITEM_URL(kurlyItem.no)}
+                text="구매"
+                target="_blank"
+                rel="noopener noreferrer"
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
