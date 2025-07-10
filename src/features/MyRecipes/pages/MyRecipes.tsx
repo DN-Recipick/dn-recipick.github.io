3825fa7fc4f2ce21f39be913d867dd908da21f69
SanghyeonLee <hyeon012366@gmail.com>
2025-07-11 03:23:33 +0900

    refactor: 불필요 버튼 제거 및 오버레이 적용

--- src/features/MyRecipes/pages/MyRecipes.tsx
+++ src/features/MyRecipes/pages/MyRecipes.tsx
@@ -1,25 +1,15 @@
-import Button from '@/components/shared/Button';
-import { ROUTES } from '@/constants/routes';
 import RecipeList from '@/features/MyRecipes/components/RecipeList';
 import useGetRecipes from '@/features/MyRecipes/hooks/useGetRecipes';
-import { Link } from 'react-router-dom';
-import { FaPlus } from 'react-icons/fa6';
 import PageLayout from '@/components/shared/PageLayout';
+import RealTimeOverlay from '@/features/MyRecipes/components/RealTimeOverlay';
 
 const MyRecipes = () => {
-  const { recipes, isPending } = useGetRecipes();
+  const { recipes, isPending, isRealTimePending } = useGetRecipes();
 
   return (
-    <PageLayout title="내 레시피">
+    <PageLayout title="저장된 레시피">
+      {isRealTimePending && <RealTimeOverlay />}
       <RecipeList isPending={isPending} recipes={recipes} />
-      <div className="fixed bottom-23 w-full layout-max-w px-content left-1/2 -translate-x-1/2 z-10 flex justify-end">
-        <Link to={ROUTES.HOME}>
-          <Button
-            icon={<FaPlus className="mini-icon-size" />}
-            className="btn-primary w-14 h-14 rounded-full opacity-90 hover:opacity-100 transition-opacity duration-200"
-          />
-        </Link>
-      </div>
     </PageLayout>
   );
 };
