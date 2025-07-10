f58ad55a19e360b49f3a13d6d00c1ee31314f716
SanghyeonLee <hyeon012366@gmail.com>
2025-07-11 03:20:49 +0900

    refactor: 탭 스타일 UX 고려해 수정

--- src/features/MyRecipeDetail/components/RecommendedRecipes.tsx
+++ src/features/MyRecipeDetail/components/RecommendedRecipes.tsx
@@ -12,20 +12,14 @@ const RecommendedRecipes = ({ id }: { id: string }) => {
     useRecommendedRecipes(id);
   return (
     <>
-      <div className="mb-5 relative flex mx-auto bg-[var(--color-secondary)] justify-center shadow-[var(--shadow)] w-full rounded-3xl overflow-hidden">
-        <div
-          className={clsx(
-            'absolute top-0 left-0 w-1/2 h-full bg-[var(--color-primary)] rounded-3xl transition-transform duration-300',
-            selectedTab === '재료관련 추천 레시피' && 'translate-x-full',
-          )}
-        />
+      <div className="pb-4 flex mx-auto justify-center w-full overflow-hidden gap-3">
         {TABS.map((tab) => (
           <Button
             key={tab}
             onClick={() => setTab(tab)}
             className={clsx(
-              'text-sm sm:text-base md:text-[1.1rem] w-1/2 h-13 z-10 transition-colors duration-200 ',
-              isSelected(tab) ? 'text-white' : 'text-[var(--color-primary)]',
+              'text-sm sm:text-base md:text-[1.1rem] shadow-[var(--shadow)] rounded-md w-1/2 h-13 z-10 transition-colors duration-200 ',
+              isSelected(tab) ? 'bg-[var(--color-primary)] text-white' : 'bg-gray-200 text-black',
             )}
             text={tab}
           />
