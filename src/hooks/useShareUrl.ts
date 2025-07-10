e94456406a494bd81d392e6f05319b0e66a966c7
SanghyeonLee <hyeon012366@gmail.com>
2025-07-11 05:48:53 +0900

    feat: 링크 공유 관련 커스텀 훅에서 에러 처리 분기

--- src/hooks/useShareUrl.ts
+++ src/hooks/useShareUrl.ts
@@ -1,25 +1,42 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { addRecipe } from '@/features/RecipeLink/api';
 import { ROUTES } from '@/constants/routes';
-import { queryKeys } from '@/constants/queryKeys';
+import { CustomError } from '@/lib/CustomError';
+import { showToast } from '@/utils/toast';
 
 export const useShareUrl = () => {
-  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
   const sharedUrl = searchParams.get('url');
   const navigate = useNavigate();
   const { mutate } = useMutation({
     mutationFn: (url: string) => addRecipe({ url }),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: queryKeys.RECIPE.all });
-      navigate(ROUTES.RECIPES);
+      console.log('success');
+      navigate(ROUTES.RECIPES, {
+        replace: true,
+        state: { fromSharedLink: true },
+      });
+    },
+    onError: (error) => {
+      console.log('error', error);
+
+      const { status, message } = error as CustomError;
+
+      const isUnauthorized = status === 401;
+
+      showToast.error(
+        isUnauthorized ? '로그인이 필요합니다.' : message || '레시피 등록 중 오류가 발생했습니다.',
+      );
+
+      navigate(ROUTES.HOME, { replace: true });
     },
   });
 
   useEffect(() => {
     if (sharedUrl) {
+      console.log('내부 실행');
       mutate(sharedUrl);
     }
   }, [sharedUrl, mutate]);
