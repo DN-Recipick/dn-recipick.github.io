1bffbc0b248e73132ca585df77260f53ec4507ee
SanghyeonLee <hyeon012366@gmail.com>
2025-07-11 03:09:03 +0900

    refactor: main 배치 수정, footer 주석

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import Header from '@/components/layout/Header';
 import { useThemeStore } from '@/store/useThemeStore';
 import { Outlet } from 'react-router-dom';
 import { useEffect } from 'react';
-import Footer from '@/components/layout/Footer';
 import Modal from '@/components/shared/Modal';
 import { useModalStore } from '@/store/useModalStore';
 import FullPageFallback from '@/components/feedback/fallback/FullPageFallback';
+import Header from '@/components/layout/Header';
 
 function App() {
   const initTheme = useThemeStore((state) => state.initTheme);
   const { isOpen } = useModalStore();
-
   useEffect(() => {
     initTheme();
   }, [initTheme]);
@@ -19,12 +17,9 @@ function App() {
     <>
       <FullPageFallback />
       <Header />
-      <main className="flex-1 w-full px-content pb-24 pt-8">
-        {/* React Router 자체적으로 에러를 감싸고 처리 (throw X) -> outlet 위에*/}
-        <Outlet />
-      </main>
+      <Outlet />
       {isOpen && <Modal />}
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
