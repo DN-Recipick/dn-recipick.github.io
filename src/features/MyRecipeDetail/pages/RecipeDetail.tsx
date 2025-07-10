60b8522d602e1ed46b9fd33ba649edfeba1d2de4
SanghyeonLee <hyeon012366@gmail.com>
2025-07-11 03:21:18 +0900

    feat: 레시피 상세 스켈레톤 적용 및 상세 ux 수정

--- src/features/MyRecipeDetail/pages/RecipeDetail.tsx
+++ src/features/MyRecipeDetail/pages/RecipeDetail.tsx
@@ -1,16 +1,14 @@
-import FullScreenLoader from '@/components/feedback/loading/FullScreenLoader';
 import { ROUTES } from '@/constants/routes';
 import Ingredients from '@/features/MyRecipeDetail/components/Ingredients';
 import RecipeProcedure from '@/features/MyRecipeDetail/components/RecipeProcedure';
 import useGetRecipeItem from '@/features/MyRecipeDetail/hooks/useGetRecipeItem';
 import { Navigate, useParams } from 'react-router-dom';
-import { formatDateTime } from '@/utils/format';
 import YoutubeVideo from '@/components/shared/YoutubeVideo';
 import Button from '@/components/shared/Button';
 import { useModalStore } from '@/store/useModalStore';
 import PageLayout from '@/components/shared/PageLayout';
-import ChannelInfo from '@/features/MyRecipeDetail/components/ChannelInfo';
 import { useAddExternalRecipe } from '@/features/MyRecipeDetail/hooks/useAddExternalRecipe';
+import SkeletonRecipeDetail from '@/components/feedback/skeleton/SkeletonRecipeDetail';
 
 const RecipeDetail = () => {
   const { open } = useModalStore();
@@ -19,25 +17,35 @@ const RecipeDetail = () => {
   const { addRecipe, isPendingAddRecipe } = useAddExternalRecipe();
 
   if (!id) return <Navigate to={ROUTES.NOT_FOUND} />;
-  if (isPendingRecipeItem || !recipeItem) return <FullScreenLoader />;
+  if (isPendingRecipeItem || !recipeItem) {
+    return (
+      <PageLayout title="레시피">
+        <SkeletonRecipeDetail />
+      </PageLayout>
+    );
+  }
 
   return (
-    <PageLayout title={recipeItem.name}>
-      <p className="date-text mb-3">저장일 : {formatDateTime(recipeItem.created_at)}</p>
+    <PageLayout title={'레시피'}>
+      <div className="mb-3 line-clamp-1 flex items-center gap-2">
+        <span className="text-2xl font-extrabold whitespace-nowrap">{recipeItem.name}</span>
+        <span className="text-[var(--color-sub-text)] text-sm whitespace-nowrap">
+          by {recipeItem.channel}
+        </span>
+      </div>
+
+      <p className="text-gray-400 mb-2 ">{recipeItem.title}</p>
+      {/* <p className="date-text mb-3">저장일 : {formatDateTime(recipeItem.created_at)}</p> */}
       <YoutubeVideo id={recipeItem.video_id} />
-      <ChannelInfo
-        videoId={recipeItem.video_id}
-        channelName={recipeItem.channel}
-        channelTitle={recipeItem.title}
-      />
       <RecipeProcedure procedure={recipeItem.item} />
       <Ingredients ingredients={recipeItem.ingredients} />
-      <Button
-        text="추천 레시피 보기"
-        className="btn-primary w-full mt-4"
-        onClick={() => open('recommendRecipes', { title: '추천 레시피', id: recipeItem.id })}
-      />
-      {!recipeItem.is_mine && (
+      {recipeItem.is_mine ? (
+        <Button
+          text="추천 레시피 보기"
+          className="btn-primary w-full mt-4"
+          onClick={() => open('recommendRecipes', { title: '추천 레시피', id: recipeItem.id })}
+        />
+      ) : (
         <Button
           text="내 레시피에 추가"
           className="btn-primary w-full mt-4"
