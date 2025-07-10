e1fc184172204a5a7cd13bfaba78ac7eb033d0c3
SanghyeonLee <hyeon012366@gmail.com>
2025-07-11 03:17:18 +0900

    feat: 인증 관련 페이지 제목 메인 내부로 수정

--- src/features/auth/components/SigninForm.tsx
+++ src/features/auth/components/SigninForm.tsx
@@ -6,29 +6,32 @@ const SigninForm = () => {
   const { isPending, errors, onSubmit, register } = useSignin();
 
   return (
-    <form onSubmit={onSubmit}>
-      <Input
-        wrapClassname="my-3"
-        className="main-input"
-        placeholder="이메일 주소를 입력하세요"
-        label="이메일"
-        error={errors.email?.message}
-        type="email"
-        {...register('email')}
-        required
-      />
-      <Input
-        wrapClassname="my-3"
-        placeholder="6자 이상 입력하세요"
-        className="main-input"
-        label="비밀번호"
-        error={errors.password?.message}
-        type="password"
-        {...register('password')}
-        required
-      />
-      <Button text="로그인" type="submit" isPending={isPending} className="btn-primary w-full" />
-    </form>
+    <>
+      <h3 className="mb-5.5">로그인</h3>
+      <form onSubmit={onSubmit}>
+        <Input
+          wrapClassname="my-3"
+          className="main-input"
+          placeholder="이메일 주소를 입력하세요"
+          label="이메일"
+          error={errors.email?.message}
+          type="email"
+          {...register('email')}
+          required
+        />
+        <Input
+          wrapClassname="my-3"
+          placeholder="6자 이상 입력하세요"
+          className="main-input"
+          label="비밀번호"
+          error={errors.password?.message}
+          type="password"
+          {...register('password')}
+          required
+        />
+        <Button text="로그인" type="submit" isPending={isPending} className="btn-primary w-full" />
+      </form>
+    </>
   );
 };
 
